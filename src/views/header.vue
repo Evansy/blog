e49a13cdff73a10ae1b825c30ec8f2ba4ab3abe3
Evan <skcy@vip.qq.com>
2017-12-01 09:56:42 +0800

    * 解决ios下滑到顶部因为弹性效果，无法显示顶部栏。

--- src/views/header.vue
+++ src/views/header.vue
@@ -77,7 +77,7 @@ export default {
             this.navbarHide = scrollTop > this.preScrollTop && (clientHeight > scrollTop + this.screenHeight);
 
             // 只要tabbar显示了就加背景，除了在顶部的时候
-            this.transBg = !this.navbarHide && scrollTop > 0;
+            this.transBg = !this.navbarHide && scrollTop > 10;
 
             // 保存本次滑动距离
             this.preScrollTop = scrollTop;
