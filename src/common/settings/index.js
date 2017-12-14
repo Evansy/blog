a855a064a1d0906057c7853000366806f3cc2100
Evan <skcy@vip.qq.com>
2017-12-15 00:31:42 +0800

    * 添加开发接口设置

--- src/common/settings/index.js
+++ src/common/settings/index.js
@@ -3,4 +3,4 @@
  */
 let debug = process.env.NODE_ENV === 'development';
 // url base
-export const apiUrl = 'http://localhost:8888';
+export const apiUrl = debug ? 'http://api.iskcy.com' : 'http://localhost:8888';
