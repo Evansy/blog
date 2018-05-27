b5b533a247660b76f22fea80ac53e646c9045d71
Evan <skcy@vip.qq.com>
2018-05-27 20:20:21 +0800

    * 修复无法重复显示畅言输入框。

--- src/common/settings/index.js
+++ src/common/settings/index.js
@@ -3,5 +3,6 @@
  */
 let debug = process.env.NODE_ENV === 'development';
 // url base
-// export const apiUrl = 'https://api.iskcy.com';
-export const apiUrl = debug ? 'http://localhost:8888' : 'https://api.iskcy.com';
+// export const apiUrl = 'http://localhost:8888';
+export const apiUrl = 'https://api.iskcy.com';
+// export const apiUrl = debug ? 'http://localhost:8888' : 'https://api.iskcy.com';
